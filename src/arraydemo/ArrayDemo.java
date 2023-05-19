0681c1420bfd9b117ff56876d02b4b6589245f00
Satnamjeet Kaur <kau14450@sheridancollege.ca>
2023-05-19 09:24:54 -0400

    first commit

---
+++ src/arraydemo/ArrayDemo.java
@@ -0,0 +1,25 @@
+
+package arraydemo;
+
+import java.util.Scanner;
+
+
+public class ArrayDemo {
+
+        public static void main(String[] args) {
+            System.out.println("Enter a word");
+            Scanner input = new Scanner(System.in);            
+            char[] myLetters = new char[myWord.length()];
+            
+            for(int i = 0; i<myLetters.length;i++){
+                myLetters[i] = myWord.charAt(i);
+                System.out.print("myLetters[i]");
+            }
+            System.out.println();
+            System.out.println("printing in reverse");
+            for(int i=myLetters.length-1;i>=0;i--){
+                System.out.print("myLetters[i]");
+            }
+    }
+    
+}
