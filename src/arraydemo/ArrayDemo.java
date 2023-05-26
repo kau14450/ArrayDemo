09f7b051cd941ffc6c581a6301cb36bdac697c09
Satnamjeet Kaur <kau14450@sheridancollege.ca>
2023-05-26 09:42:18 -0400

    Added part-time-student class

--- src/arraydemo/ArrayDemo.java
+++ src/arraydemo/ArrayDemo.java
@@ -19,6 +19,7 @@ public class ArrayDemo {
             System.out.println("printing in reverse");
             for(int i=myLetters.length-1;i>=0;i--){
                 System.out.print("myLetters[i]");
+                //changes made
             }
     }
     
